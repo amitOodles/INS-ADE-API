7c4b619713ecff5a754c94f9535f04f591f9a91d
amitOodles <amit.k@oodlestechnologies.com>
2016-10-04 13:51:55 +0530

    changes in comments

--- server.js
+++ server.js
@@ -57,55 +57,7 @@ app.get('/query', function(req, res) {
 app.post('/webshot', function(req, res, callback){
 
     postData = req.body;
-    // console.log(postData);
 
-//      postData = {
-// 	"age":50,
-// 	"grossAnnualIncome":120000,
-// 	"funeralCost":20000,
-// 	"familyLivingCostPerYear":90000,
-// 	"hasSpouse":true,
-// 	"hasChildren":true,
-// 	"sickLeaves" : 20,
-// 	"assets" : {
-// 		"homeValue" : 800000,
-// 		"cashAtBank" : 20000,
-// 		"otherInvestment" :20000,
-// 		"superBalance" : 100000
-// 	},
-// 	"existingCovers":{
-// 		"life" : 20000,
-// 		"TPD" : 0,
-// 		"IP" : 0,
-// 		"trauma" : 0
-// 	},
-// 	"assumptions":{
-// 		"inflation" : 2,
-// 		"rateOfReturn" : 5
-// 	},
-// 	"liabilities":{
-// 		"homeMortgage" : 20000,
-// 		"investmentPropertyMortgage" : 10000,
-// 		"creditCardDebt" : 3000,
-// 		"carLoan" : 20000,
-// 		"personalLoan" : 10000,
-// 		"otherLoan" : 0
-// 	},
-// 	"spouseDetails":{
-// 		"age":47,
-// 		"isWorking":true,
-// 		"salary":50000,
-// 		"moveToSmallerProperty":true,
-// 		"valueOfNewProperty" : 500000,
-// 		"moneyToBeBorrowed":400000
-// 	},
-// 	"childrenDetails":{
-// 		"numChildren":2,
-// 		"ages":[3,7],
-// 		"educationExpensePerYearPerChild":2000
-// 	}
-// };
-    // console.log("server",postData);
 
     var timeS = new Date;
     var name = timeS.getTime() + ".png";
@@ -115,7 +67,7 @@ app.post('/webshot', function(req, res, callback){
             // res.write("error saving");
 
             if (err) {
-                var resErr = new Error("Unable to generate SSO chart");
+                var resErr = new Error("Unable to generate Insurance Adequacy chart");
                 resErr.status = 400;
                 console.log("error occured", resErr);
                 callback(resErr);
